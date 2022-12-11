6d522012d79cbae1181c8385e943fe238dea1d15
tugbacanbaz <tugbacanbaz.pi@gmail.com>
2022-12-11 23:59:04 +0300

    Complited Assignment

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-// Add your code here
+const submitData = (userName, UserEmail) => {
+    
+    const formData = {
+      name: userName,
+      email: UserEmail,
+    };
+    
+    fetch('http://localhost:3000/users', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        Accept: 'application/json',
+      },
+      body: JSON.stringify(formData),
+    })
+
+       .then((response) => {
+     return  response.json();
+    })
+    .then((object) => {
+      document.querySelector("body").append(object.id);
+    })
+      .catch(function (error) {
+        alert("Error!");
+        document.querySelector("body").innerHTML = error.message
+        console.log(error.message);
+      });
+  };
+  
+
+  submitData('user', 'user@gmail.com');
